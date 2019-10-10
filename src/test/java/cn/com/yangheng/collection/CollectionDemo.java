2a3018deaecf85c4d1bb02af19a5dd9ccb02f0ad
yangheng <740018154@qq.com>
2019-10-10 23:09:43 +0800

    10.10号实现arrayList和linkList

---
+++ src/test/java/cn/com/yangheng/collection/CollectionDemo.java
@@ -0,0 +1,12 @@
+package cn.com.yangheng.collection;
+
+import java.util.*;
+
+public class CollectionDemo {
+
+    public void test(){
+        Set hashSet =new HashSet();
+        List arrayList=new ArrayList<>();
+        List linkedList = new LinkedList();
+    }
+}

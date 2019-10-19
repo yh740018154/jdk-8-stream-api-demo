33b89afd19a854862622a2c2564c56955eca4783
yangheng <yangheng@dragonsoft.com.cn>
2019-10-19 16:56:24 +0800

    10.11，lamda表达式学习

--- src/test/java/cn/com/yangheng/lamda/TestLamda.java
+++ src/test/java/cn/com/yangheng/lamda/TestLamda.java
@@ -40,4 +40,6 @@ public class TestLamda {
         List<Integer> nums = Lists.newArrayList(1,1,null,2,3,4,null,5,6,7,8,9,10);
         System.out.println("sum is :"+nums.stream().filter(num -> num != null).distinct().mapToInt(num -> num * 2).peek(System.out::println).skip(2).limit(4).sum());
     }
+
+
 }
